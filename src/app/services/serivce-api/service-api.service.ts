27742f48e1992ae31e54b657f2a87fad5fcb2d5d
pure1234567a <pure1234567a@gmail.com>
2019-02-01 11:31:05 +0700

    service

---
+++ src/app/services/serivce-api/service-api.service.ts
@@ -0,0 +1,43 @@
+
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceApiService {
+
+  constructor(
+    public http: HttpClient
+  ) { }
+
+  saveUser(body) {
+    return this.http.post(environment.apiUrl + '/api/auth/signin', body).toPromise()
+  }
+
+  getProd() {
+    return this.http.get(environment.apiUrl + '/api/productsrabu').toPromise()
+  }
+
+  getProdById(_id) {
+    return this.http.get(environment.apiUrl + '/api/productsrabu', _id).toPromise()
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
